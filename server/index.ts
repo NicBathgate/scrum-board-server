f050702f16db65eacaf3fe7f6332950fe62a880a
Nic Bathgate <nic.bathgate@tracplus.com>
2020-06-12 11:24:44 +1200

    nothing to see here...

--- server/index.ts
+++ server/index.ts
@@ -53,17 +53,14 @@ const callbackBaseUrl = process.env.APP_ROOT_URL || `http://localhost:${port}`;
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        process.env.APP_OAUTH_CLIENT_ID ||
-        "126647422471-2t7pfb8rll2aic63hpgccjnuqjoidjj6.apps.googleusercontent.com",
-      clientSecret:
-        process.env.APP_OAUTH_CLIENT_SECRET || "W22ouUd81nbGDIsJmhIjr9o-",
+      clientID: process.env.APP_OAUTH_CLIENT_ID,
+      clientSecret: process.env.APP_OAUTH_CLIENT_SECRET,
       callbackURL: callbackBaseUrl + "/api/auth/google/callback",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     (accessToken, refreshToken, profile, done) => {
-      // TODO: might need to add this back in to check user has adinstruments.com email?
-      // if (profile._json.hd !== "adinstruments.com") {
+      // TODO: might need to add this back in to check user has tracplus.com email?
+      // if (profile._json.hd !== "tracplus.com") {
       //   https.get(
       //     "https://accounts.google.com/o/oauth2/revoke?token=" + accessToken,
       //     function(res) {}
